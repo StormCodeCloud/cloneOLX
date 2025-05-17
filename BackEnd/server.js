f1f3ec8319436ddee0d8c2b856574456eeff7a9e
StormCodeCloud <cloudstorm0705@gmail.com>
2025-05-17 15:47:35 +0100

    conection query via sequelize [Done]

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -10,11 +10,32 @@ const hostname = "127.0.0.1";
 app.use(express.json());
 
 // Sequelize configuration
-const sequelize = new Sequelize("ficha9", "root", "duarte2003", {
+const sequelize = new Sequelize("cloneOLX", "root", "password", {
   host: "localhost",
   dialect: "mysql",
-  port: 3307,
+  port: 3306,
 });
+
+// Test the database connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+async function createDatabase() {
+  try {
+    await sequelize.query("CREATE DATABASE IF NOT EXISTS cloneOLX");
+    console.log("Database created successfully.");
+  } catch (error) {
+    console.error("Error creating database:", error);
+  }
+}
 
 // Start the server
 app.listen(port, () => {
